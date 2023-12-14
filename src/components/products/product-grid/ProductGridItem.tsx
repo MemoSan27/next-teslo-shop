c9499e788eea1459e881de82d63559d27a897355
MemoSan27 <135660314+MemoSan27@users.noreply.github.com>
2023-12-14 14:43:58 -0700

    product card in develop2

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -10,16 +10,21 @@ interface Props{
 export const ProductGridItem = ({ product }: Props) => {
   return (
     <div className="rounded-md overflow-hidden fade-in">
-        <Image 
-            src={`/products/${ product.images[0] }`}
-            alt={ product.title }
-            className="w-full object-cover"
-            width={500}
-            height={500}
-        />
+        <Link href={`/product/${ product.slug }`}>
+          <Image 
+              src={`/products/${ product.images[0] }`}
+              alt={ product.title }
+              className="w-full object-cover"
+              width={500}
+              height={500}
+          />
+        </Link> 
 
         <div className="p-4 flex flex-col">
-          <Link href={`/product/${ product.slug }`}>
+          <Link 
+            className="hover:text-blue-600"
+            href={`/product/${ product.slug }`}
+          >
             { product.title }
           </Link>
           <span className="font-bold">${ product.price }</span>
